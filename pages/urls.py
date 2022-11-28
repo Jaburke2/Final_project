89e60c90e800fff8a2ceea6fcf5df3eac7c350b0
Joshua Burke <Jaburke2@buffs.wtamu.edu>
2022-11-27 22:07:59 -0600

    update

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 #pages/ur;s.py
 from django.urls import path
 
-from .views import homePageView
+from .views import HomePageView, AboutPageView
+
 
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 # pages/Views.py
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+# def homePageView(request):
+#    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
